23b67b562551bc776077316d08728327f48f2be6
Rick282003 <richardgalo2003@gmail.com>
2025-07-24 19:31:31 -0600

    Testimonios

--- siteData/index.js
+++ siteData/index.js
@@ -1,5 +1,6 @@
 import cotizacionData from './pages/cotizacion-servicio';
 import formularioData from './pages/formulario'
+import testimoniosMainData from './pages/testimoniosMain';
 
 function context(page){
     console.log("Getting context for: " + page);
@@ -7,6 +8,9 @@ function context(page){
         title: "Mi Sitio ABC",
     }
     switch(page){
+        case "/index.html":
+            context = {...context, ...testimoniosMainData};
+            break;
         case '/cotizaciones.html':
             context = {...context, ...cotizacionData, ...formularioData};
             break;
---
+++ siteData/pages/testimoniosMain.js
@@ -0,0 +1,14 @@
+export default{
+    testimoniosMainData:[
+        {
+            imagen: "images/reparador.jpg",
+            testimonio: "La mejor experiencia de trabajo que he tenido.",
+            nombre: "Luiz David, plomero"
+        },
+        {
+            imagen: "https://picsum.photos/id/49/480/480",
+            testimonio: "Trabajo muy bien pagado, sin demora alguna, un espacio de trabajo agradable",
+            nombre: "Perez Guitierrez, roof"
+        },
+    ]
+}
