6262a5247868c3f9fa6f479511532b1efd1784be
Rick282003 <richardgalo2003@gmail.com>
2025-07-27 01:37:37 -0600

    Imagenes en testimonios

--- siteData/pages/testimoniosMain.js
+++ siteData/pages/testimoniosMain.js
@@ -1,22 +1,22 @@
 export default {
     testimoniosMainData: [
         {
-            imagen: "images/reparador.jpg",
+            imagen: "images/techos.jpg",
             testimonio: "He trabajado en muchas compañías, pero aquí valoran nuestro esfuerzo. Siempre hay trabajo y el pago es puntual.",
             nombre: "Luis David, especialista en techos"
         },
         {
-            imagen: "https://picsum.photos/id/49/480/480",
+            imagen: "images/siding.jpg",
             testimonio: "Lo que más me gusta es el ambiente. Somos como una familia y siempre hay apoyo entre compañeros.",
             nombre: "Pedro Gutiérrez, instalador de siding"
         },
         {
-            imagen: "https://picsum.photos/id/50/480/480",
+            imagen: "images/carpintero.jpg",
             testimonio: "Empecé como un simple ayudante y ahora soy jefe de equipo. Aquí hay oportunidades reales para crecer.",
             nombre: "Roberto Hernández, carpintero"
         },
         {
-            imagen: "https://picsum.photos/id/51/480/480",
+            imagen: "images/remodelador.jpg",
             testimonio: "La empresa siempre nos da las herramientas y materiales que necesitamos. Eso facilita mucho el trabajo.",
             nombre: "Javier Ríos, técnico remodelador"
         },
