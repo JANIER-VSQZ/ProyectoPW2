b41320667f8b391866fbaf8b43392daa2fcf7189
Rick282003 <richardgalo2003@gmail.com>
2025-07-27 00:56:24 -0600

    Testimonios Reales

--- siteData/pages/testimoniosMain.js
+++ siteData/pages/testimoniosMain.js
@@ -1,19 +1,24 @@
-export default{
-    testimoniosMainData:[
+export default {
+    testimoniosMainData: [
         {
             imagen: "images/reparador.jpg",
-            testimonio: "La mejor experiencia de trabajo que he tenido.",
-            nombre: "Luiz David, plomero"
+            testimonio: "He trabajado en muchas compañías, pero aquí valoran nuestro esfuerzo. Siempre hay trabajo y el pago es puntual.",
+            nombre: "Luis David, especialista en techos"
         },
         {
             imagen: "https://picsum.photos/id/49/480/480",
-            testimonio: "Trabajo muy bien pagado, sin demora alguna, un espacio de trabajo agradable",
-            nombre: "Perez Guitierrez, roof"
+            testimonio: "Lo que más me gusta es el ambiente. Somos como una familia y siempre hay apoyo entre compañeros.",
+            nombre: "Pedro Gutiérrez, instalador de siding"
         },
         {
             imagen: "https://picsum.photos/id/50/480/480",
-            testimonio: "Tengo Hambre",
-            nombre: "Diego Hernández, carpintero"
-        }
+            testimonio: "Empecé como un simple ayudante y ahora soy jefe de equipo. Aquí hay oportunidades reales para crecer.",
+            nombre: "Roberto Hernández, carpintero"
+        },
+        {
+            imagen: "https://picsum.photos/id/51/480/480",
+            testimonio: "La empresa siempre nos da las herramientas y materiales que necesitamos. Eso facilita mucho el trabajo.",
+            nombre: "Javier Ríos, técnico remodelador"
+        },
     ]
 }
