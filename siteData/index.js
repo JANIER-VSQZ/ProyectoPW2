e2f0177f8761b881c5a9705a2c6a7b854b57fe88
DiegoHernandez987 <dihernandez987@unicah.edu>
2025-07-27 22:19:21 -0600

    galeria1

--- siteData/index.js
+++ siteData/index.js
@@ -3,6 +3,7 @@ import formularioData from './pages/formulario'
 import testimoniosMainData from './pages/testimoniosMain';
 import heroData from './pages/heroImagenes';
 import serviciosData from './pages/servicios';
+import galeriaData from './pages/galeria';
 
 function context(page){
     console.log("Getting context for: " + page);
@@ -18,6 +19,10 @@ function context(page){
             break;
         case '/servicios.html':
             context = {...context, ...serviciosData};
+            break;
+        case '/galeria.html':
+            context = {...context, ...galeriaData};
+            break;
     }
     console.log("Context: ", JSON.stringify(context, null, 2));
     return context;
---
+++ siteData/pages/galeria.js
@@ -0,0 +1,37 @@
+export default{
+    Paisajes: [
+        {
+            imgLink: 'https://picsum.photos/id/323/400/267',
+            titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
+        },
+        {
+            imgLink: 'https://picsum.photos/id/324/400/267',
+            titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
+        },
+        {
+            imgLink: 'https://picsum.photos/id/327/400/267',
+            titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
+        },
+        {
+            imgLink: 'https://picsum.photos/id/337/400/267',
+            titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
+        },
+        {
+            imgLink: 'https://picsum.photos/id/350/400/267',
+            titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
+        },
+        {
+            imgLink: 'https://picsum.photos/id/379/400/267',
+            titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
+        },
+        {
+            imgLink: 'https://picsum.photos/id/412/400/267',
+            titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
+        },
+        
+        {
+            imgLink: 'https://picsum.photos/id/433/400/267',
+            titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
+        },
+    ]
+}
