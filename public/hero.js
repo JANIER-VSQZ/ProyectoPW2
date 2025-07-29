fd7fad9d114360e6efc641432f3bb6d3b1081096
Rick282003 <richardgalo2003@gmail.com>
2025-07-28 19:33:38 -0600

    Variable

--- public/hero.js
+++ public/hero.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     let currentIndex = 0;
-    const images = document.querySelectorAll('.hero-imagen img');
-    const total = images.length;
+    const imagenes = document.querySelectorAll('.hero-imagen img');
+    const total = imagenes.length;
 
     setInterval(() => {
         currentIndex = (currentIndex + 1) % total;
-        images.forEach((img, index) => {
+        imagenes.forEach((img, index) => {
             img.style.transform = `translateX(-${currentIndex * 100}%)`;
         });
     }, 3000);
