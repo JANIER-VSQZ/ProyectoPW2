4a3a652ed230fb70258187d15ee66d9e42ca627f
DiegoHernandez987 <dihernandez987@unicah.edu>
2025-07-29 22:50:23 -0600

    ya...creo

--- siteData/pages/galeria.js
+++ siteData/pages/galeria.js
@@ -4,37 +4,37 @@ export default {
         nombrePag: "Galeria",
         imagenData: [
             {
-                imgLink: '/images/ace4.png',
-                titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
+                imgLink: '/images/a6.jpg',
+                titulo: 'Técnicos en escalera reparando un ventanal en el segundo piso de una casa con revestimiento beige y zócalo de piedra.'
             },
             {
-                imgLink: '/images/ace3.png',
-                titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
+                imgLink: '/images/a7.jpg',
+                titulo: 'Operario con casco y andamio instalando molduras en la fachada azul de una vivienda.'
             },
             {
-                imgLink: '/images/ace3.png',
-                titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
+                imgLink: '/images/a8.jpg',
+                titulo: 'Trabajador con guantes verdes retirando tejas dañadas rojas y negras durante la reparación de un techo.'
             },
             {
-                imgLink: '/images/ace4.png',
-                titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
-            },
-            {
-                imgLink: '/images/ace2.png',
-                titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
+                imgLink: '/images/a9.jpg',
+                titulo: 'Profesional con casco y chaleco reflectante ajustando tejas en una cubierta, destacando el uso de equipo de seguridad.'
             },
             {
                 imgLink: '/images/ace1.png',
-                titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
+                titulo: 'En la fase inicial se observa el esqueleto de la cubierta con vigas de madera alineadas y sujetas con arneses de seguridad, mientras el equipo comprueba nivelación y anclajes provisionales antes del sellado definitivo.'
             },
             {
-                imgLink: 'https://picsum.photos/id/412/400/267',
-                titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
+                imgLink: '/images/ace2.png',
+                titulo: 'Aplicamos primario asfáltico Karnak y soldamos membrana bituminosa con soplete, sellando juntas y detalles de tuberías y respiraderos para una impermeabilización duradera.'
+            },
+            {
+                imgLink: '/images/ace3.png',
+                titulo: 'En este trabajo identificamos la madera original con grietas, agujeros y signos avanzados de pudrición. Se retiraron las piezas dañadas, reforzamos las vigas con madera tratada y nivelamos la estructura.'
             },
 
             {
-                imgLink: 'https://picsum.photos/id/433/400/267',
-                titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
+                imgLink: '/images/ace4.png',
+                titulo: 'Instalamos tableros contrachapados marinos de 15 mm, fijados con tornillería galvanizada y arandelas de neopreno para garantizar estanqueidad y durabilidad. El resultado es una base sólida, uniforme y preparada para la siguiente fase de cubrición.'
             },
         ]
     },
@@ -43,32 +43,28 @@ export default {
         nombrePag: "Herramientas",
         imagenData: [
             {
-                imgLink: 'https://picsum.photos/id/740/400/267',
-                titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
+                imgLink: '/images/h1.webp',
+                titulo: 'Nivel láser: proyecta líneas de luz horizontales o verticales para alinear con precisión.'
             },
             {
-                imgLink: 'https://picsum.photos/id/740/400/267',
-                titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
+                imgLink: '/images/h2.jpg',
+                titulo: 'Sierra ingleteadora: disco giratorio ajustable que realiza cortes angulados exactos en madera.'
             },
             {
-                imgLink: 'https://picsum.photos/id/740/400/267',
-                titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
+                imgLink: '/images/h3.jpg',
+                titulo: 'Clavadora neumática: dispara clavos con aire comprimido para fijaciones rápidas.'
             },
             {
-                imgLink: 'https://picsum.photos/id/740/400/267',
-                titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
+                imgLink: '/images/h5.png',
+                titulo: 'Vibrador de concreto: inserta vibraciones en el hormigón fresco para compactarlo y expulsar aire.'
             },
             {
-                imgLink: 'https://picsum.photos/id/740/400/267',
-                titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
+                imgLink: '/images/h6.png',
+                titulo: 'Llana de acero inoxidable: herramienta manual para alisar y dar acabado uniforme al mortero.'
             },
             {
-                imgLink: 'https://picsum.photos/id/740/400/267',
-                titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
-            },
-            {
-                imgLink: 'https://picsum.photos/id/740/400/267',
-                titulo: 'Trigal dsfgdgtreg hreth rte h rth rt h erh ert h rt h erth er th y u gt5 h yhj rt h 56 h5t h yrt y 5t hgrt hg 5 hj56 h 56 h 56 hr h 56 y56 56h 5uj 56 hj56 5 hj 56j5'
+                imgLink: '/images/h7.png',
+                titulo: 'Pistola de calafateo: empuja sellador desde un cartucho, aplicando cordones continuos y controlados.'
             },
         ]
     }
