64d0ed7c32b34a05add0cad620054a15b270af21
Daglos <102191213+Daglos@users.noreply.github.com>
2025-07-29 20:57:09 -0600

    Pagina Nosotros terminada sujeta revision

---
+++ siteData/pages/acercaNosotros.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const slides = document.querySelectorAll(".img-slide");
+  let index = 0;
+
+ 
+  slides[index].classList.add("active");
+
+  setInterval(() => {
+    // esconde img
+    slides[index].classList.remove("active");
+
+    // prepara la siguiente
+    index = (index + 1) % slides.length;
+
+    // enseña la nueva
+    slides[index].classList.add("active");
+  }, 4000);
+});
