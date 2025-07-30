6ac603ef656f50271566837b9fbb6cadf0fff91d
Emelyram <emem_ramirezv@unicah.edu>
2025-07-29 23:24:30 -0600

    testimonios

--- siteData/pages/testimoniosclientes.js
+++ siteData/pages/testimoniosclientes.js
@@ -1,49 +1,49 @@
 const testimonios = [
   {
     imagen: "images/mujer1.jpg",
-    testimonio: "El producto superó mis expectativas. La entrega fue rápida y el servicio amable.",
+    testimonio: "¡Trabajo impecable! La constructura superó nuestras expectativas. Entregaron el Proyecto a tiempo y con calidad.",
     nombre: "Ana Martínez",
     estrellas: "★★★★★"
   },
   {
     imagen: "images/hombre1.jpeg",
-    testimonio: "Muy satisfecha con la calidad y atención. Recomiendo mucho esta empresa.",
+    testimonio: "Desde el primer día mostraron profesionalismo y compromiso. Nos acompañaron en cada proceso y el resultado fue el deseado.",
     nombre: "Carlos López",
     estrellas: "★★★★"
   },
   {
     imagen: "images/mujer2.jpg",
-    testimonio: "El equipo fue muy profesional y resolvieron todas mis dudas con paciencia.",
+    testimonio: "Excelente atención al cliente, materiales de primera y un equipo altamente capacitado. Se nota que les apasiona lo que hacen.",
     nombre: "Lucía Rivas",
     estrellas: "★★★"
   },
   {
     imagen: "images/hombre2.jpg",
-    testimonio: "Excelente experiencia, volveré a comprar sin duda alguna.",
+    testimonio: "Estamos encantados con nuestra nueva casa. Todo el proceso fue transparente, organizado y sin sorpresas. Una experiencia positiva",
     nombre: "Pedro Aguilar",
     estrellas: "★★★★★"
   },
   {
     imagen: "images/mujer3.jpg",
-    testimonio: "Productos frescos y de alta calidad, además de un servicio muy atento.",
+    testimonio: "Lo que más valoro fue su capacidad de escuchar mis ideas y convertirlas en realidad. Se adaptaron a mi creatividad.",
     nombre: "Mariela Cruz",
     estrellas: "★★★★★"
   },
   {
     imagen: "images/hombre3.jpeg",
-    testimonio: "Muy buena atención al cliente y entrega puntual. Totalmente recomendados.",
+    testimonio: "La constructora demostró ser confiable y cumplida. Nos mantuvieron informados constantemente y resolvieron cualquier duda.",
     nombre: "José Ramírez",
     estrellas: "★★★★"
   },
   {
     imagen: "images/hombre4.jpeg",
-    testimonio: "Debes vivir tranquilo estando en un lugar con garantía que no se caerá, ellos lo ofrecen.",
+    testimonio: "Hicieron un buen trabajo extraordinario con los acabados. Cuidaron cada detalle como si fuera su propio hogar.",
     nombre: "Noel Barahona",
     estrellas: "★★★★★"
   },
   {
     imagen: "images/señora4.jpg",
-    testimonio: "Son buenos contrustuctores como personas y eso es una característica buena.",
+    testimonio: "Recomiendo esta empresa sin dudarlo. Su responsabilidad, experiencia y calidad humana hicieron toda la diferencia en mi proyecto.",
     nombre: "Olga Benitez",
     estrellas: "★★★★"
   }
